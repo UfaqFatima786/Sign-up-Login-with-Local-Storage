cb74722404a1414b1ff8c130809c2577ac9f8850
UfaqFatima786 <u.capricorn19@gmail.com>
2026-04-29 11:27:29 +0500

    login signup

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
- function tosignup(){
-window.location.href="signup.html"
- }
-  function tologin(){
-window.location.href="login.html"
- }
+function signup() {
+  var signupEmail = document.getElementById("signup-email").value;
+  var signupPassword = document.getElementById("signup-password").value;
+  console.log(signupEmail, signupPassword);
+  
+  if (signupEmail == "" || signupPassword == "") {
+    Swal.fire({
+      icon: "warning",
+      title: "Missing Fields",
+      text: "Please enter email and password"
+    });
+    return;
+  }
+
+  localStorage.setItem("signupEmail", signupEmail);
+  localStorage.setItem("signupPassword", signupPassword);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function tosignup() {
+  window.location.href = "signup.html"
+}
+function tologin() {
+  window.location.href = "login.html"
+}
